b2fb4140511ccb09fef42b6104f776a686570122
DmitryPatskalev <dimanord@yandex.ru>
2023-02-08 20:56:18 +0300

    01_Lesson_Part_01

---
+++ src/nav/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Nav.module.css'
+
+export const Nav = () => {
+   return (
+     <div className={s.nav}>
+        <a href="" className="href">Main</a>
+        <a href="" className="href">Skills</a>
+        <a href="" className="href">Projects</a>
+        <a href="" className="href">Contact</a>
+     </div>
+   );
+};
+
