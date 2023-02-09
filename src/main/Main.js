375bd57e6aa86e41d7abeb94d57f0e14133dfdbf
DmitryPatskalev <dimanord@yandex.ru>
2023-02-09 07:16:45 +0300

    01_Lesson_Part_02

---
+++ src/main/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import s from './Main.module.css'
+import style from './../common/styles/Container.module.css'
+
+export const Main = () => {
+   return (
+     <div className={s.mainBlock}>
+        <div className={style.container}>
+           <div className={s.text}>
+              <span>Hi There</span>
+              <h1>I am Loki, prince of Asgard, son of Odin!</h1>
+              <p>Front End Developer</p>
+           </div>
+           <div className={s.photo}>
+           </div>
+        </div>
+     </div>
+   );
+};
+
